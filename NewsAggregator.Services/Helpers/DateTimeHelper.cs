b54156ee4837f0a3965ce6e6a0e7908c1834cadc
Adrian Flis <adrianflis23@gmail.com>
2022-04-11 16:48:16 +0200

    add datetime helper

---
+++ NewsAggregator.Services/Helpers/DateTimeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NewsAggregator.Services.Helpers
+{
+    public static class DateTimeHelper
+    {
+        public static DateTime AbsoluteStart(this DateTime dateTime)
+        {
+            return dateTime.Date;
+        }
+
+        public static DateTime AbsoluteEnd(this DateTime dateTime)
+        {
+            return AbsoluteStart(dateTime).AddDays(1).AddMilliseconds(-1);
+        }
+    }
+}
