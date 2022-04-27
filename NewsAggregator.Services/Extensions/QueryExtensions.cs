8d33d3bb6dedd51219e618464f1138c3cd677dac
Adrian Flis <adrianflis23@gmail.com>
2022-04-27 16:16:29 +0200

    add extensions to iqueryable of rssarticle to paginate results

---
+++ NewsAggregator.Services/Extensions/QueryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using NewsAggregator.Data.Models;
+using NewsAggregator.Services.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsAggregator.Services.Extensions
+{
+    public static class QueryExtensions
+    {
+        public static IQueryable<RssArticle> Paginate(
+            this IQueryable<RssArticle> input,
+            PaginationFilter paginationFilter
+        )
+        {
+            return input
+                .OrderByDescending(x => x.DateTime)
+                .Skip((paginationFilter.PageNumber - 1) * paginationFilter.PageSize)
+                .Take(paginationFilter.PageSize);
+        }
+    }
+}
