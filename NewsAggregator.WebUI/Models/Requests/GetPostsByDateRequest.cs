8a199f0db33d7026e713391998216292263acacb
Adrian Flis <adrianflis23@gmail.com>
2022-04-28 14:34:18 +0200

    remove unused request models

--- NewsAggregator.WebUI/Models/Requests/GetPostsByDateRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace NewsAggregator.WebUI.Models.Requests
-{
-    public class GetPostsByDateRequest
-    {
-        public DateTime Date { get; set; }
-    }
-}
--- NewsAggregator.WebUI/Models/Requests/UpdateCategoryKeywordsRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace NewsAggregator.WebUI.Models.Requests
-{
-    public class UpdateCategoryKeywordsRequest
-    {
-        public List<string> Keywords { get; set; }
-    }
-}
